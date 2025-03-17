684ac19a782f21c1595115b96fea235753e1513d
Luccas Asaphe <167210535+LuccasAps@users.noreply.github.com>
2025-03-17 11:12:55 -0300

    MainView controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.luccasaps.workshopjavafxjdbc {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens com.luccasaps.workshopjavafxjdbc to javafx.fxml;
     exports com.luccasaps.workshopjavafxjdbc;
