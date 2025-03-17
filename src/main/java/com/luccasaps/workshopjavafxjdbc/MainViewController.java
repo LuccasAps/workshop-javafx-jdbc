9b54b04c83a90935cbcbab13a1e2f19258732030
Luccas Asaphe <167210535+LuccasAps@users.noreply.github.com>
2025-03-17 11:47:47 -0300

    About view

--- src/main/java/com/luccasaps/workshopjavafxjdbc/MainViewController.java
+++ src/main/java/com/luccasaps/workshopjavafxjdbc/MainViewController.java
@@ -1,10 +1,19 @@
 package com.luccasaps.workshopjavafxjdbc;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import util.Alerts;
+
 import javax.swing.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -31,10 +40,28 @@ public class MainViewController implements Initializable{
 
     @FXML
     public void onMenuItemAboutAction() {
-        System.out.println("onMenuItemAboutAction");
+        loadView("about.fxml");
     }
 
     @Override
     public void initialize(URL uri, ResourceBundle rb) {
+    }
+
+    private synchronized void loadView(String absoluteName){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVbox = loader.load();
+
+            Scene mainScene = Main.getMainScene();
+            VBox mainVBox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
+
+            Node mainMenu = mainVBox.getChildren().get(0);
+            mainVBox.getChildren().clear();
+            mainVBox.getChildren().add(mainMenu);
+            mainVBox.getChildren().addAll(newVbox.getChildren());
+
+        }catch (IOException e){
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
