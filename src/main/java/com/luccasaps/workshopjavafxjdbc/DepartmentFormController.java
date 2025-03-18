927eb517e21330cdcc78f5ef42b2d9d118399463
Luccas Asaphe <167210535+LuccasAps@users.noreply.github.com>
2025-03-18 11:23:47 -0300

    Salvando um novo departamento

--- src/main/java/com/luccasaps/workshopjavafxjdbc/DepartmentFormController.java
+++ src/main/java/com/luccasaps/workshopjavafxjdbc/DepartmentFormController.java
@@ -1,10 +1,15 @@
 package com.luccasaps.workshopjavafxjdbc;
 
+import com.luccasaps.workshopjavafxjdbc.db.DbException;
 import com.luccasaps.workshopjavafxjdbc.model.entities.Department;
+import com.luccasaps.workshopjavafxjdbc.model.services.DepartmentService;
+import com.luccasaps.workshopjavafxjdbc.util.Alerts;
 import com.luccasaps.workshopjavafxjdbc.util.Constraints;
+import com.luccasaps.workshopjavafxjdbc.util.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -15,6 +20,8 @@ import java.util.ResourceBundle;
 public class DepartmentFormController implements Initializable {
 
     private Department entity;
+
+    private DepartmentService service;
 
     @FXML
     private TextField txtId;
@@ -35,15 +42,40 @@ public class DepartmentFormController implements Initializable {
         this.entity = entity;
     }
 
+    public void setDepartmentService(DepartmentService service) {
+        this.service = service;
+    }
+
 
     @FXML
-    public void onBtSaveAction() {
-        System.out.println("onBtSaveAction");
+    public void onBtSaveAction(ActionEvent event) {
+        if(entity == null) {
+            throw new IllegalStateException("Entity was null");
+        }
+        if(service == null) {
+            throw new IllegalStateException("Service was null");
+        }
+        try {
+            entity = getFormData();
+            service.saveOrUpdate(entity);
+            Utils.currentStage(event).close();
+        }
+        catch(DbException e){
+            Alerts.showAlert("Error saving object", null, e.getMessage(), Alert.AlertType.ERROR);
+        }
+    }
+
+    private Department getFormData() {
+        Department obj = new Department();
+        obj.setId(Utils.tryParseToInt(txtId.getText()));
+        obj.setName(txtName.getText());
+
+        return obj;
     }
 
     @FXML
-    public void onBtCancelAction() {
-        System.out.println("onBtCancelAction");
+    public void onBtCancelAction(ActionEvent event) {
+        Utils.currentStage(event).close();
     }
 
     @Override
